4824bfa5462ff5421d0ff8a6b96fb1866674aecb
0-Xerife <bruno.barroso@polijunior.com.br>
2024-09-18 18:20:15 -0300

    :bug: component adjusts

--- src/components/ItemCard/index.tsx
+++ src/components/ItemCard/index.tsx
@@ -5,26 +5,20 @@ interface ItemCardProps {
     item: {
         id: string;
         name: string;
-        points: number;
+        percentage: number;
     };
-    onEditClick: (item: { id: string; name: string; points: number }) => void;
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ item, onEditClick }) => {
+const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
     return (
         <S.Card>
-            <S.EditIcon
-                src="assets/icons/coracao.svg"
-                alt="Edit"
-                onClick={() => onEditClick(item)}
-            />
             <S.Content>
                 <S.Image src="assets/images/luva.jpg" />
             </S.Content>
             <S.Footer>
                 <S.ItemName>{item.name}</S.ItemName>
                 <S.Points>
-                    Pontos <span>{item.points}</span>
+                    Porcentagem: <span>{item.percentage}%</span>
                 </S.Points>
             </S.Footer>
         </S.Card>
